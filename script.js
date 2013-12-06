d047b7bc190815fc99debe90362f10e5d7821e08
Kotaro Kawashima <utageyanagi+kkgithub@gmail.com>
2013-12-06 15:04:11 +0900

    /ASIN/に対応してない問題を修正

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 var params=location.pathname.split("/");
-var dppos = params.indexOf("dp");
-if(dppos == -1){
-  dppos = params.indexOf("product");
-}
+var dppos = params.indexOf("dp") + 1 || params.indexOf("product") + 1 || params.indexOf("ASIN") + 1;
 
-if(dppos == -1){
+
+if(dppos === 0){
 	console.log("No ASIN code");
 	return;
 }
 
-var asin = params[dppos + 1];
+var asin = params[dppos];
 var url = "http://amazon.jp/dp/"+asin+"/"+window.amzn.affiliate.account;
 
 console.log(url);
