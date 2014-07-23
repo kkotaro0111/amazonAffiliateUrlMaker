d46eb9019d91a7ad710111e83ed70e928863ebd3
Kotaro Kawashima <utageyanagi+kkgithub@gmail.com>
2014-07-23 19:50:13 +0900

    functionじゃないのにreturnすんなって怒られたの修正

--- script.js
+++ script.js
@@ -3,32 +3,31 @@ var dppos = params.indexOf("dp") + 1 || params.indexOf("product") + 1 || params.
 
 
 if(dppos === 0){
-	console.log("No ASIN code");
-	return;
+  console.log("No ASIN code");
+}else{
+  var asin = params[dppos];
+  var url = "http://amazon.jp/dp/"+asin+"/"+window.amzn.affiliate.account;
+
+  console.log(url);
+
+  var form = $("#asamashi");
+  if( form.length === 0){
+    var form = $("<input />");
+    form.attr({
+      type: "text",
+      id: "asamashi",
+      size: url.length + 5,
+      readonly: true
+    }).css({
+      position: "fixed",
+      right: 0,
+      bottom: 0
+    }).on("focus",function(e){
+      this.select();
+    });
+
+    form.appendTo("body");
+  }
+
+  form.val( url );
 }
-
-var asin = params[dppos];
-var url = "http://amazon.jp/dp/"+asin+"/"+window.amzn.affiliate.account;
-
-console.log(url);
-
-var form = $("#asamashi");
-if( form.length === 0){
-  var form = $("<input />");
-  form.attr({
-    type: "text",
-    id: "asamashi",
-    size: url.length + 5,
-    readonly: true
-  }).css({
-    position: "fixed",
-    right: 0,
-    bottom: 0
-  }).on("focus",function(e){
-    this.select();
-  });
-
-  form.appendTo("body");
-}
-
-form.val( url );
